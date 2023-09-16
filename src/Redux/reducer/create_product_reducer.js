04c76a29b7ae40f4c3542b295bb19166f95c08ec
facundoc1 <facundocayo1@gmail.com>
2023-09-15 21:01:41 -0300

    Cloudinary para form, adaptacion actions y reducer

--- src/Redux/reducer/create_product_reducer.js
+++ src/Redux/reducer/create_product_reducer.js
@@ -1,41 +1,39 @@
 import {
-    CREATE_PRODUCT_REQUEST,
-    CREATE_PRODUCT_SUCCESS,
-    CREATE_PRODUCT_FAILURE,
-  } from '../actions/actions_create_product'; 
-  
+  CREATE_PRODUCT_REQUEST,
+  CREATE_PRODUCT_SUCCESS,
+  CREATE_PRODUCT_FAILURE,
+} from '../types/types';
 
 const initialState = {
-    product: null,
-    loading: false,
-    error: null,
-  };
-  
-  const createProductReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case CREATE_PRODUCT_REQUEST:
-        return {
-          ...state,
-          loading: true,
-          error: null,
-        };
-      case CREATE_PRODUCT_SUCCESS:
-        return {
-          ...state,
-          product: action.payload.product,
-          loading: false,
-          error: null,
-        };
-      case CREATE_PRODUCT_FAILURE:
-        return {
-          ...state,
-          loading: false,
-          error: action.payload.error,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default createProductReducer;
-  
+  product: null,
+  loading: false,
+  error: null,
+};
+
+const createProductReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_PRODUCT_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case CREATE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        product: action.payload.product,
+        loading: false,
+        error: null,
+      };
+    case CREATE_PRODUCT_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+      };
+    default:
+      return state;
+  }
+};
+
+export default createProductReducer;
