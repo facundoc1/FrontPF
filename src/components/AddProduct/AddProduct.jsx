f4848328fb794a8f77161321db99c30284bdd89d
facundoc1 <facundocayo1@gmail.com>
2023-09-20 21:51:12 -0300

    arreglo cloud, card parcial

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { createProduct } from '../../Redux/actions/actions_create_product';
 import { getUserProfile } from '../../Redux/actions/actions_profile';
+import { Image } from 'cloudinary-react';
+import { CloudinaryContext } from 'cloudinary-react';
 
 const CreateProduct = () => {
   const dispatch = useDispatch();
@@ -10,37 +12,54 @@ const CreateProduct = () => {
     summary: '',
     price: 0,
     stock: 0,
-    images: [], 
+    images: '',
     categoryIds: '',
     subcategoryIds: '',
   });
 
   const userProfile = useSelector((state) => state.profile.userData);
 
-  const loading = useSelector((state) => state.createProduct.loading);
-  const error = useSelector((state) => state.createProduct.error);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProductData({
+      ...productData,
+      [name]: value,
+    });
+  };
 
   useEffect(() => {
     dispatch(getUserProfile());
   }, [dispatch]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
   
-    if (name === 'images') {
-      // Dividir las URLs separadas por comas en un array
-      const imagesArray = value.split(',').map((url) => url.trim());
-      setProductData({
-        ...productData,
-        [name]: imagesArray,
-      });
-    } else {
-      setProductData({
-        ...productData,
-        [name]: value,
-      });
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'GRTECH1');
+  
+    try {
+      const response = await fetch(
+        'https://api.cloudinary.com/v1_1/dvjef49et/image/upload',
+        {
+          method: 'POST',
+          body: formData,
+        }
+      );
+  
+      if (response.ok) {
+        const data = await response.json();
+        setProductData({
+          ...productData,
+          images: data.secure_url,
+        });
+      }
+    } catch (error) {
+      console.error('Error al cargar la imagen:', error);
     }
   };
+
+   
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -52,19 +71,8 @@ const CreateProduct = () => {
       };
 
       try {
-  
-        console.log('productData:', productData);
-        console.log('productData.images:', productData.images);
-        
-        if (productData.images.length === 0) {
-          console.error('Debes seleccionar al menos una imagen.');
-          return;
-        }
-
         const formData = new FormData();
-        productData.images.forEach((image, index) => {
-          formData.append(`image${index}`, image);
-        });
+        formData.append('images', productData.images);
         formData.append('title', productData.title);
         formData.append('summary', productData.summary);
         formData.append('price', productData.price);
@@ -80,9 +88,9 @@ const CreateProduct = () => {
           summary: '',
           price: 0,
           stock: 0,
-          images: [],
-          categoryIds: '',
-          subcategoryIds: '',
+          images: '',
+          categoryIds: '', 
+          subcategoryIds: '', 
         });
       } catch (error) {
         console.error('Error al crear el producto:', error);
@@ -93,11 +101,9 @@ const CreateProduct = () => {
     }
   };
 
-
   return (
     <div>
       <h2>Crear nueva publicación de producto</h2>
-      {error && <p>Error: {error.message}</p>}
       <form onSubmit={handleSubmit}>
         <div>
           <label>Título:</label>
@@ -116,15 +122,15 @@ const CreateProduct = () => {
           <input type="number" name="stock" value={productData.stock} onChange={handleChange} required />
         </div>
         <div>
-        <label>Imágenes (URLs separadas por comas):</label>
-        <input
-            type="text"
-            name="images"
-            value={productData.images}
-            onChange={handleChange}
-            required
-            />
-        </div>
+          <label>Cargar Imágenes:</label>
+          <input
+            type="file"
+            name="image"
+            onChange={handleImageUpload}
+            multiple
+          />
+        </div>        
+        
         <div>
           <label>Categoría ID (separados por comas):</label>
           <input
@@ -143,11 +149,21 @@ const CreateProduct = () => {
             onChange={handleChange}
           />
         </div>
-        <button type="submit" disabled={loading}>Crear Producto</button>
+       
+        <div>
+        <CloudinaryContext cloudName="dvjef49et">
+        {productData.images && (
+  <div>
+    <Image publicId={productData.images} width="100" height="100" crop="fill" />
+  </div>
+)}
+          </CloudinaryContext>
+        </div>
+
+        <button type="submit">Crear Producto</button>
       </form>
     </div>
   );
 };
 
 export default CreateProduct;
-
